7796f50a1eef751c57eac8c4335263d41487a92f
Daniel Kunz <exip@gmx.net>
2014-05-12 20:32:53 +0200

    halbwegs brauchbares Grundgerüst

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   match '/home',    to: 'static_pages#home',    via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
-  
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
